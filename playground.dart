9315fe1464689316af6628634a83a44e3525173b
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-11-30 16:55:05 +0600

    Switch Conditional Statement

--- playground.dart
+++ playground.dart
@@ -1,13 +1,16 @@
-// Conditional Statement
+// Switch Conditional Statement
 
 void main() {
-  int number = 91;
+  int number = 20;
 
-  if (number % 2 == 0) {
-    print('Even');
-  } else if (number % 3 == 0) {
-    print('Odd');
-  } else {
-    print('Confused');
+  switch (number) {
+    case 0:
+      print('Even');
+      break;
+    case 1:
+      print('Odd');
+      break;
+    default:
+      print('Confused');
   }
 }

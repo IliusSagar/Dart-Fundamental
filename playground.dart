19ba7e7c687887c2ba16763ac8436971aa094fa6
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-11-30 13:48:06 +0600

    data types

--- playground.dart
+++ playground.dart
@@ -1,7 +1,34 @@
-import 'dart:io';
-
 void main() {
-  stdout.writeln('What is your name: ?');
-  String name = stdin.readLineSync()!;
-  print('My name is $name');
+  /*
+  int
+  double
+  String
+  bool
+  dynamic
+  */
+
+  int amount1 = 100;
+  var amount2 = 200;
+  print('Amount1: $amount1 | Amount2: $amount2 \n');
+
+  double dAmount1 = 100.11;
+  var dAmount2 = 200.22;
+  print('dAmount1: $dAmount1 | dAmount2: $dAmount2 \n');
+
+  String name1 = 'ilius';
+  var name2 = 'sagar';
+  print('My name is: $name1 $name2 \n');
+
+  bool isItTrue1 = true;
+  var isItTrue2 = false;
+  print('isItTrue1: $isItTrue1 | isItTrue2: $isItTrue2 \n');
+
+  dynamic weakVariable = 100;
+  print('WeakVariable 1: $weakVariable \n');
+
+  weakVariable = 'Dart Programming';
+  print('WeakVariable 2: $weakVariable');
+
+  weakVariable = null;
+  print(weakVariable);
 }

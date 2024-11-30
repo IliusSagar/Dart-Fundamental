96f3a9541c4de7c40a01ad60e6a8dbd932833183
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-11-30 16:36:45 +0600

    3 variations of operators

--- playground.dart
+++ playground.dart
@@ -1,34 +1,19 @@
+// Null Aware Operator
+// (?.), (??), (??=)
+
+class Num {
+  int num = 10;
+}
+
 void main() {
-  int num = 10 + 22;
-  num = num - 2;
-  print(num);
+  var n = Num();
+  int? number;
 
-  num = num % 5;
-  print(num);
+  // if (n != null) {
+  //   number = n.num;
+  // }
 
-  // relational ==, !=, >=, <=
-  if (num == 0) {
-    print('Zero');
-  }
+  number = n?.num ?? 0;
 
-  num = 100;
-  num *= 2; // num = num * 2
-  print(num);
-
-  // unary operator
-  ++num;
-  num++;
-  num += 1;
-  num -= 1;
-  print(num);
-
-  // logical && and logical ||
-  if (num > 200 && num < 203) {
-    print('200 to 202');
-  }
-
-  // != Not Equal
-  if (num != 100) {
-    print('num is not equal to 100');
-  }
+  print(number);
 }

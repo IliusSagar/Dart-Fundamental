a955001b348a952b1cd2bb3265ec21ca0cbf108a
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-11-30 14:16:06 +0600

    automatically asign null value

--- playground.dart
+++ playground.dart
@@ -1,13 +1,4 @@
 void main() {
-  const aConstNum = 0; // int constant
-  const aConstBool = true; // bool constant
-  const aConstString = 'a constant string'; // string constant
-
-  print(aConstNum);
-  print(aConstBool);
-  print(aConstString);
-
-  print(aConstNum.runtimeType);
-  print(aConstBool.runtimeType);
-  print(aConstString.runtimeType);
+  int? num;
+  print(num);
 }

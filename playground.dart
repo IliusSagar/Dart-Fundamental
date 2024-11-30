3975e0983e7037df90cfd9cdf9408f4c0266b67e
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-11-30 17:02:19 +0600

    for-in loop

--- playground.dart
+++ playground.dart
@@ -1,8 +1,14 @@
 // Loop
 
 void main() {
-  // Standard for loop
-  for (var i = 1; i <= 10; ++i) {
-    print(i);
+  // for-in loop
+  var numbers = [1, 2, 3];
+
+  for (var n in numbers) {
+    print(n);
+  }
+
+  for (var i = 0; i < numbers.length; ++i) {
+    print(numbers[i]);
   }
 }

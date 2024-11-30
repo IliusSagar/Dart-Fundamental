dfc4b9133ced7b1c18460cc15ec09184ee0de41d
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-11-30 10:47:43 +0600

    stdout & stdin

--- playground.dart
+++ playground.dart
@@ -1,6 +1,7 @@
-main() {
-  var firstName = "ilius";
-  String lastName = "sagar";
+import 'dart:io';
 
-  print(firstName + ' ' + lastName);
+void main() {
+  stdout.writeln('What is your name: ?');
+  String name = stdin.readLineSync()!;
+  print('My name is $name');
 }

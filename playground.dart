7e29fcdc092b71a12973fa27192b7280f7592dd8
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-12-01 13:11:22 +0600

    type will be defined by inferred value

--- playground.dart
+++ playground.dart
@@ -1,29 +1,17 @@
 // Class
 
-class Person {
-  String name;
-  int age;
+class X {
+  final name; // type will be defined by inferred value
+  static const int age = 10;
 
-  Person(this.name, [this.age = 18]);
-
-  // named constructor
-  Person.guest()
-      : name = 'Guest',
-        age = 18; // Initialize non-nullable fields directly
-
-  void showOutput() {
-    print(name);
-    print(age);
-  }
+  X(this.name);
 }
 
 void main() {
-  Person person1 = Person('Sagar');
-  person1.showOutput();
+  var x = X('Jack');
+  print(x.name);
+  print(X.age);
 
-  var person2 = Person('jack', 25);
-  person2.showOutput();
-
-  var person3 = Person.guest();
-  person3.showOutput();
+  var y = X('Jill');
+  print(y.name);
 }
